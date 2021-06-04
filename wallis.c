9e05c78efef96775a38b082011bad97b70a58aac
sanjeevgi <85180664+sanjeevgi@users.noreply.github.com>
2021-06-04 11:39:37 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -27,10 +27,11 @@ int main(void) {
     int i;
     float k,ans=1.0;
     for (i=1;i<=n;i++)
-    }
-  k=(float)(4*i*i)/(4*i*i-1);
-  ans=ans*k;
+    
+    {
+      k=(float)(4*i*i)/(4*i*i-1);
+      ans=ans*k;
 }
-return(2*ans);
+    return(2*ans);
 }
 
